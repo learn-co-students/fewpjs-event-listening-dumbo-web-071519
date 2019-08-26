bb6b8a0fc23eb954ef29b70c651c1425b4875e0e
Megan <MeganOliviaWendyAdams@gmail.com>
2019-08-26 19:41:45 -0400

    Done.

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 function addingEventListener() {
+
+    document.getElementById('input')
+input.addEventListener('click', function(event) {
+    alert('Hi!');
+});
+
+
 }
+
+addingEventListener('#input');
+
+
+
+
+
+
+
